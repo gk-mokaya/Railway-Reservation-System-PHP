caa56574aab6394bd3a2880de8ed14695ff0d49e
gk-mokaya <kevinmokaya001@gmail.com>
2024-11-07 08:00:02 +0300

    initial commit

---
+++ employee/assets/inc/footer.php
@@ -0,0 +1,4 @@
+<div class="splash-footer">
+    &copy; 2019 - <?php echo date ('Y');?>
+    Developed By Mokaya Kevin :- Contact: 0790886755/0797252133 E-mail: mcpymokaya@gmail.com
+</div>
---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE HTML>
+<!--
+	Online Railway Reservation System
+	martdevelopers254 | @MartinMbithi
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>CENTRAL HAMBURG RAILWAY BOOKING</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="includes/css/main.css" />
+		<noscript><link rel="stylesheet" href="includes/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload" >
+
+		<!-- Page Wrapper -->
+			<div id="page-wrapper" style="background-color: green;">
+
+				<!-- Header -->
+					<header id="header" class="alt">
+						<h1><a href="index.php">CENTRAL HAMBURG RAILWAY BOOKING</a></h1>
+						<nav>
+							<a href="#menu">Menu</a>
+						</nav>
+					</header>
+
+				<!-- Menu -->
+					<nav id="menu">
+						<div class="inner">
+							<h2>Menu</h2>
+							<ul class="links">
+								<li><a href="index.php">Home</a></li>
+								<li><a href="pass-login.php">Make Reservation</a></li>
+								<li><a href="employee/emp-login.php">Employee Login</a></li>
+								<li><a href="admin/emp-login.php">Admin Login</a></li>
+							</ul>
+							<a href="#" class="close">Close</a>
+						</div>
+					</nav>
+
+				<!-- Banner -->
+					<section id="banner">
+						<div class="inner">
+							<div class="logo"><span class="icon solid fa-train"></span></div>
+							<h2>CENTRAL HAMBURG RAILWAY BOOKING</h2>
+							<p>Best, Simple, and User Friendly Way To Reserve Train Tickets Effectively</a></p>
+						</div>
+					</section>
+
+					<section id="footer">
+						<div class="inner">
+							<h2 class="major">Get in touch</h2>
+							
+							<ul class="contact">
+								<li class="icon solid fa-home">
+								CENTRAL HAMBURG RAILWAY BOOKING<br />
+									
+								</li>
+								<li class="icon solid fa-phone">(+254) 790886755</li>
+								<li class="icon solid fa-envelope"><a href="#">centralhamburg@gmail.com</a></li>
+								<li class="icon brands fa-twitter"><a href="#">twitter.com/centralhamburg</a></li>
+								<li class="icon brands fa-facebook-f"><a href="#">facebook.com/centralhamburg</a></li>
+								<li class="icon brands fa-instagram"><a href="#">instagram.com/centralhamburg</a></li>
+							</ul>
+							<ul class="copyright">
+								<li>&copy; centralhamburg.</li><li>Developed By: Mokaya Kevin, Omolo Stanley, Abayo Leslie & Omondi Charles </li>
+							</ul>
+						</div>
+					</section>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="includes/js/jquery.min.js"></script>
+			<script src="includes/js/jquery.scrollex.min.js"></script>
+			<script src="includes/js/browser.min.js"></script>
+			<script src="includes/js/breakpoints.min.js"></script>
+			<script src="includes/js/util.js"></script>
+			<script src="includes/js/main.js"></script>
+
+	</body>
+</html>
